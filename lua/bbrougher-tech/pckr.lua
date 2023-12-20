e5abe3b2fab01a9248abfbfa452e00c2c1562680
Ben Brougher <bbrougher@ceriumnetworks.com>
2023-12-19 16:50:47 -0800

    adding additional autocomplete features

--- lua/bbrougher-tech/pckr.lua
+++ lua/bbrougher-tech/pckr.lua
@@ -95,6 +95,8 @@ require("pckr").add({
 			-- Autocompletion
 			{ "hrsh7th/nvim-cmp" }, -- Required
 			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-buffer" },
 			{ "L3MON4D3/LuaSnip" }, -- Required
 			{ "saadparwaiz1/cmp_luasnip" },
 		},
