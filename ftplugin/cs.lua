d1a95bfe9760d45390cf3b59b15cf8ae7da57233
Ben Brougher <Ben Brougher>
2024-07-12 16:49:43 -0700

    fix: logging line add in ftplugins

--- ftplugin/cs.lua
+++ ftplugin/cs.lua
@@ -1,6 +1,7 @@
 vim.keymap.set("n", "<leader>sl", function()
-    vim.api.nvim_put({"fmt.Println()"}, "", true, true)
-    local cursor = vim.api.nvim_win_get_cursor(0)
+    vim.api.nvim_put({"Console.WriteLine();"}, "", true, true)
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    vim.api.nvim_win_set_cursor(0, { row, col - 1 })
     vim.cmd("startinsert")
 end)
 
