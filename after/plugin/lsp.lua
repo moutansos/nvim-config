e8f5398cbfac1ab5a2cdb6ab8205ca4d7e2305f1
Ben Brougher <Ben Brougher>
2025-06-04 12:32:48 -0700

    fix: removed deprecated setup_handlers call

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,24 +21,6 @@ require("mason-lspconfig").setup({
     },
 })
 
-require("mason-lspconfig").setup_handlers({
-    -- Will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function(server_name) -- default handler (optional)
-        -- https://github.com/neovim/nvim-lspconfig/pull/3232
-        if server_name == "tsserver" then
-            server_name = "ts_ls"
-        end
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-        })
-    end,
-})
-
--- Fix Undefined global 'vim'
--- lsp.nvim_workspace() -- Deprecated since 2.x
-
 local cmp = require("cmp")
 local cmp_format = require("lsp-zero").cmp_format()
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
