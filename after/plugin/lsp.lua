0f95fa7786976d999f857d5555bc493e4aba8c5d
Ben Brougher <moutansos@gmail.com>
2025-07-31 10:32:26 -0700

    fix: get vue working

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,32 +47,26 @@ end
 --   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 -- }
 
-vim.lsp.config['ts_ls'] = {
-    cmd = { "typescript-language-server", "--stdio" },
-    capabilities = capabilities,
-    init_options = {
-        hostInfo = "neovim",
-    },
-    on_attach = on_attach,
-    root_pattern = { "package.json", "tsconfig.json" },
-    filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact" },
-}
+-- vim.lsp.config["ts_ls"] = {
+--     cmd = { "typescript-language-server", "--stdio" },
+--     capabilities = capabilities,
+--     init_options = {
+--         hostInfo = "neovim",
+--         plugins = {
+--             {
+--                 name = "@vue/typescript-plugin",
+--                 location = vim.fn.stdpath("data")
+--                     .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+--                 languages = { "vue" },
+--             },
+--         },
+--     },
+--     on_attach = on_attach,
+--     root_pattern = { "package.json", "tsconfig.json" },
+--     filetypes = { "typescript", "typescriptreact", "typescript.tsx", "javascript", "javascriptreact", "vue" },
+-- }
 
-
-
--- lspconfig.csharp_ls.setup({
---     root_dir = function(startpath)
---         return lspconfig.util.root_pattern("*.sln")(startpath)
---             or lspconfig.util.root_pattern("*.csproj")(startpath)
---             or lspconfig.util.root_pattern(".git")(startpath)
---     end,
---     -- handlers = {
---     --     ["textDocument/definition"] = require('csharpls_extended').handler,
---     --     ["textDocument/typeDefinition"] = require('csharpls_extended').handler,
---     -- },
--- })
-
-vim.lsp.config('lua_ls', {
+vim.lsp.config("lua_ls", {
     cmd = { "lua-language-server" },
     capabilities = capabilities,
     on_attach = on_attach,
@@ -99,7 +93,7 @@ vim.lsp.config('lua_ls', {
     filetypes = { "lua" },
 })
 
-vim.lsp.config('yamlls', {
+vim.lsp.config("yamlls", {
     cmd = { "yaml-language-server", "--stdio" },
     capabilities = capabilities,
     on_attach = on_attach,
@@ -116,11 +110,11 @@ vim.lsp.config('yamlls', {
                 ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
                 ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
                 ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
-                    "*api*.{yml,yaml}",
+                "*api*.{yml,yaml}",
                 ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
-                    "*docker-compose*.{yml,yaml}",
+                "*docker-compose*.{yml,yaml}",
                 ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] =
-                    "*flow*.{yml,yaml}",
+                "*flow*.{yml,yaml}",
             },
         },
     },
@@ -128,15 +122,48 @@ vim.lsp.config('yamlls', {
 
 vim.lsp.config("gopls", {
     on_attach = on_attach,
+    capabilities = capabilities,
 })
 
-vim.lsp.enable('ts_ls')
-vim.lsp.enable('csharp_ls')
-vim.lsp.enable('htmx')
-vim.lsp.enable('jsonls')
-vim.lsp.enable('yamlls')
-vim.lsp.enable('lua_ls')
-vim.lsp.enable('gopls')
+local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+local vue_plugin = {
+  name = '@vue/typescript-plugin',
+  location = vue_language_server_path,
+  languages = { 'vue' },
+  configNamespace = 'typescript',
+}
+local vtsls_config = {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          vue_plugin,
+        },
+      },
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+}
+
+local vue_ls_config = {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+vim.lsp.config('vtsls', vtsls_config)
+vim.lsp.config('vue_ls', vue_ls_config)
+
+-- vim.lsp.enable("ts_ls")
+vim.lsp.enable("csharp_ls")
+vim.lsp.enable("htmx")
+vim.lsp.enable("jsonls")
+vim.lsp.enable("yamlls")
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("gopls")
+
+vim.lsp.enable({'vtsls', 'vue_ls'})
 
 vim.diagnostic.config({
     virtual_text = true,
