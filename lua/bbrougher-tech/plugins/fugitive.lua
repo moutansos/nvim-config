91769430b3c2be4ef8e6aaf187f2714ca84f46ad
Ben Brougher <Ben Brougher>
2026-02-11 22:20:56 -0800

    feat: add git add all and git add this commands

--- lua/bbrougher-tech/plugins/fugitive.lua
+++ lua/bbrougher-tech/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
     "tpope/vim-fugitive",
     cmd = { "Git", "G" },
     keys = {
-        { "<leader>ga", ":Git add .<CR>", desc = "Git Add" },
+        { "<leader>gaa", ":Git add .<CR>", desc = "Git Add All Files" },
+        { "<leader>gat", ":Git add %<CR>", desc = "Git Add This File" },
     },
 }
