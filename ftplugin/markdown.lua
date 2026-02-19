947b45ec2e4a7666d92a8407538cefd114ca57f8
Ben Brougher <moutansos@gmail.com>
2026-02-19 13:50:54 -0800

    feat: added file opening in markdown files

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,16 +1,23 @@
 local function replaceInLine(lineNum)
+    local cursorCol = vim.api.nvim_win_get_cursor(0)[2]
     local line = vim.api.nvim_buf_get_lines(0, lineNum - 1, lineNum, false)[1]
     if line:find("%[%s%]") ~= nil then
         --replace with checked markdown
         local replacedString = string.gsub(line, "%[%s%]", "[x]")
         -- print("Replaced string at " .. line .. ": " .. replacedString)
-        vim.api.nvim_win_set_cursor(0, {lineNum, 0})
+        vim.api.nvim_win_set_cursor(0, {lineNum, cursorCol})
+        vim.api.nvim_set_current_line(replacedString)
+    elseif line:find("%[%-%]") ~= nil then
+        --replace with checked markdown
+        local replacedString = string.gsub(line, "%[%-%]", "[x]")
+        -- print("Replaced string at " .. line .. ": " .. replacedString)
+        vim.api.nvim_win_set_cursor(0, {lineNum, cursorCol})
         vim.api.nvim_set_current_line(replacedString)
     elseif line:find("%[x%]") ~= nil then
         --replace with unchecked markdown
         local replacedString = string.gsub(line, "%[x%]", "[ ]")
         -- print("Replaced string at " .. line .. ": " .. replacedString)
-        vim.api.nvim_win_set_cursor(0, {lineNum, 0})
+        vim.api.nvim_win_set_cursor(0, {lineNum, cursorCol})
         vim.api.nvim_set_current_line(replacedString)
     end
 end
@@ -35,6 +42,16 @@ vim.keymap.set("v", "<leader>x", function()
         replaceInLine(lineNum)
     end
 end)
+
+local os_name = vim.loop.os_uname().sysname
+
+if os_name == "Windows_NT" then
+    vim.keymap.set("n", "<leader>of", ":!start <cfile><CR>", { silent = true })
+elseif os_name == "Linux" then
+    -- Linux-specific configuration
+elseif os_name == "Darwin" then
+    -- macOS-specific configuration
+end
 
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
