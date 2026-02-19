8dac5fbcca37551a72e5403d786d2c5fbe8140a3
Ben Brougher <moutansos@gmail.com>
2026-02-19 09:50:27 -0800

    smarter md task checking/unchecking

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,17 +1,41 @@
-vim.keymap.set("n", "<leader>x", function()
-    local line = vim.api.nvim_get_current_line()
-    if (line:find("%[%s%]") ~= nil) then
+local function replaceInLine(lineNum)
+    local line = vim.api.nvim_buf_get_lines(0, lineNum - 1, lineNum, false)[1]
+    if line:find("%[%s%]") ~= nil then
         --replace with checked markdown
         local replacedString = string.gsub(line, "%[%s%]", "[x]")
-        print("Replaced string: " .. replacedString)
+        print("Replaced string at " .. line .. ": " .. replacedString)
+        vim.api.nvim_win_set_cursor(0, {lineNum, 0})
         vim.api.nvim_set_current_line(replacedString)
-    elseif (line:find("%[x%]") ~= nil) then
+    elseif line:find("%[x%]") ~= nil then
         --replace with unchecked markdown
         local replacedString = string.gsub(line, "%[x%]", "[ ]")
-        print("Replaced string: " .. replacedString)
+        print("Replaced string at " .. line .. ": " .. replacedString)
+        vim.api.nvim_win_set_cursor(0, {lineNum, 0})
         vim.api.nvim_set_current_line(replacedString)
     end
+end
+
+vim.keymap.set("n", "<leader>x", function()
+    local lineNum = vim.api.nvim_win_get_cursor(0)[1]
+    replaceInLine(lineNum)
 end)
+
+vim.keymap.set("v", "<leader>x", function()
+    local line_start = vim.fn.line("v")
+    local line_end = vim.fn.line(".")
+
+    if line_start > line_end then
+        line_start, line_end = line_end, line_start
+    end
+
+    print("Line start: " .. line_start)
+    print("Line end: " .. line_end)
+
+    for _, lineNum in ipairs(vim.fn.range(line_start, line_end)) do
+        replaceInLine(lineNum)
+    end
+end)
+
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
     pattern = "*.md",
