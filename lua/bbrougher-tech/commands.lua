b1957481b117ea6f5eac9ae04d777dbc26c265ce
Ben Brougher <Ben Brougher>
2026-06-12 21:31:38 -0700

    fix: move to newer version of the treesitter parser

--- lua/bbrougher-tech/commands.lua
+++ lua/bbrougher-tech/commands.lua
@@ -1,16 +1,24 @@
 function startsWith(text, prefix)
-    return text:find(prefix, 1, true) == 1
+	return text:find(prefix, 1, true) == 1
 end
 
-vim.api.nvim_create_user_command('GG',
-    function(opts)
-        git_cmd = opts.fargs[1]
-        cwd = vim.loop.cwd()
-        if vim.fn.has('wsl') == 1 and startsWith(cwd, "/mnt/") then
-            vim.cmd('split term://git.exe ' .. git_cmd)
-        else
-            vim.cmd('split term://git ' .. git_cmd)
-        end
-    end,
-    { nargs = 1 }
-)
+vim.api.nvim_create_user_command("GG", function(opts)
+	git_cmd = opts.fargs[1]
+	cwd = vim.loop.cwd()
+	if vim.fn.has("wsl") == 1 and startsWith(cwd, "/mnt/") then
+		vim.cmd("split term://git.exe " .. git_cmd)
+	else
+		vim.cmd("split term://git " .. git_cmd)
+	end
+end, { nargs = 1 })
+
+-- Start tree-sitter highlighting for all file types, but wrap in pcall to avoid errors if a parser isn't installed
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("EnableTreesitterHighlighting", { clear = true }),
+	desc = "Enable tree-sitter syntax highlighting",
+	pattern = "*",
+	callback = function()
+		-- Wrap in pcall to avoid errors if a parser isn't installed
+		pcall(vim.treesitter.start)
+	end,
+})
