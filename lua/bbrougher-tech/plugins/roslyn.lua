3224e2da2f4122d2038dcc509eb9f216036d22f1
Ben Brougher <moutansos@gmail.com>
2026-06-18 10:08:14 -0700

    fix: switch to roslyn file watching

--- lua/bbrougher-tech/plugins/roslyn.lua
+++ lua/bbrougher-tech/plugins/roslyn.lua
@@ -1,6 +1,9 @@
 return {
     "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
     opts = {
+        filewatching = "roslyn",
         settings = {
             ["csharp|backgroundAnalysis"] = {
                 -- Ensures background analysis runs for the entire solution, not just open documents
