a67be17ae09010a0bb3d14043cf621b0e0a273be
Ben Brougher <moutansos@gmail.com>
2026-08-09 18:22:35 +0000

    fix: nvim treesitter

--- lua/bbrougher-tech/plugins/treesitter.lua
+++ lua/bbrougher-tech/plugins/treesitter.lua
@@ -8,33 +8,8 @@ vim.api.nvim_create_autocmd("User", {
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
+        branch = "main",
         build = ":TSUpdate",
         lazy = false,
-        main = "nvim-treesitter.configs",
-        opts = {
-            ensure_installed = {
-                "javascript",
-                "typescript",
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-                "markdown",
-                "markdown_inline",
-            },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        },
     },
 }
