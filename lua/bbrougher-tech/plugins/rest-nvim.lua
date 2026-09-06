129afed4f00233bc157ebda7320d000ac8b96048
moutansos <moutansos@gmail.com>
2026-09-06 21:53:34 +0000

    fix: start treesitter highlighting on nvim-treesitter main
    The main branch only ships parsers and queries; highlighting is
    provided by Neovim and is not enabled automatically. Add a FileType
    autocmd calling vim.treesitter.start() and install the parsers
    explicitly, since ensure_installed/auto_install no longer exist.
    
    Drop the stale ensure_installed hook from the rest.nvim spec and
    install the http parser from the nvim-treesitter spec instead.

--- lua/bbrougher-tech/plugins/rest-nvim.lua
+++ lua/bbrougher-tech/plugins/rest-nvim.lua
@@ -1,11 +1,8 @@
 return {
     "rest-nvim/rest.nvim",
     dependencies = {
+        -- The `http` parser is installed by the nvim-treesitter spec.
         "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            opts.ensure_installed = opts.ensure_installed or {}
-            table.insert(opts.ensure_installed, "http")
-        end,
     },
     keys = {
         {
--- lua/bbrougher-tech/plugins/treesitter.lua
+++ lua/bbrougher-tech/plugins/treesitter.lua
@@ -10,6 +10,42 @@ return {
         "nvim-treesitter/nvim-treesitter",
         branch = "main",
         build = ":TSUpdate",
+        -- The `main` branch does not support lazy-loading.
         lazy = false,
+        config = function()
+            -- Parser names, not filetypes. `:TSInstall <Tab>` lists them all.
+            require("nvim-treesitter").install({
+                "bash",
+                "blade",
+                "c",
+                "c_sharp",
+                "css",
+                "go",
+                "gosum",
+                "html",
+                "http",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "php",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+            })
+
+            -- Highlighting is opt-in on the `main` branch: Neovim provides it,
+            -- the plugin only ships parsers and queries. Start it for every
+            -- buffer whose language has a parser available.
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    pcall(vim.treesitter.start, args.buf)
+                end,
+            })
+        end,
     },
 }
